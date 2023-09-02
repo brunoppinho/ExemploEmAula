5feaf8978d07472d8c51cd44a7d3015d2efb929a
bruno-pinho-alpe <bruno.pinho@alpenet.com.br>
2023-09-01 21:45:07 -0300

    Fim da aula;

---
+++ src/main/java/br/com/pinho/cursos/java/oo/exemplo/model/ContaCorrente.java
@@ -0,0 +1,21 @@
+package br.com.pinho.cursos.java.oo.exemplo.model;
+
+import java.math.BigDecimal;
+
+public class ContaCorrente extends Conta {
+
+    private BigDecimal limite;
+
+    public BigDecimal getLimite() {
+        return limite;
+    }
+
+    public void setLimite(BigDecimal limite) {
+        this.limite = limite;
+    }
+
+    public boolean possoRetirarOValor(BigDecimal valor) {
+        return this.getSaldo().add(limite).compareTo(valor) > 0;
+    }
+
+}
